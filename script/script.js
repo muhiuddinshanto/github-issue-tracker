749ce310afa4e9a28e4609bd3c2db7230b4bc86c
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-03-09 13:18:11 +0600

    api call success & all issue Display

---
+++ script/script.js
@@ -0,0 +1,164 @@
+const cardSection =  document.getElementById('card-section');
+const issuCount = document.getElementById('issue-count');
+const allBtn = document.getElementById("btn-all")
+const openBtn = document.getElementById("btn-open")
+const closedBtn = document.getElementById("btn-closed")
+let allIssues = [];
+
+
+
+
+function getLabelClass(label){
+    if(label.toLowerCase() === "bug") return "badge badge-soft badge-error";          
+    if(label.toLowerCase() === "enhancement") return "badge badge-soft badge-success"; 
+    if(label.toLowerCase() === "help wanted") return "badge badge-soft badge-warning"; 
+    if(label.toLowerCase() === "good first issue") return "badge badge-soft badge-info"; 
+    return "badge badge-soft badge-secondary"; 
+}
+
+function getLabelImage(label){
+    label = label.toLowerCase();
+    if(label === "bug") return "./assets/Vector.png";
+    if(label === "enhancement") return "./assets/Sparkle.png";
+    if(label === "help wanted") return "./assets/Vector (1).png";
+    return "";
+}
+
+
+
+
+async  function allIssueLoad (){
+
+   await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
+   .then ((res)=>  res.json())
+   .then ((issue) => {
+       allIssues =issue.data;
+       allIssueDisplay(issue.data)
+       console.log(allIssues);
+   }
+   )
+   
+}
+
+/*
+{
+    "id": 1,
+    "title": "Fix navigation menu on mobile devices",
+    "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+    "status": "open",
+    "labels": [
+        "bug",
+        "help wanted"
+    ],
+    "priority": "high",
+    "author": "john_doe",
+    "assignee": "jane_smith",
+    "createdAt": "2024-01-15T10:30:00Z",
+    "updatedAt": "2024-01-15T10:30:00Z"
+}
+*/
+
+
+
+
+const allIssueDisplay = (issue)=>{
+    cardSection.innerHTML = "";
+    issue.forEach(element => {
+
+const borderCOlor = element.status === "open"
+        ? "border-[#00A96E]"
+        : "border-[#A855F7]";
+
+const openClosedIcon = element.status === "open"
+        ? "./assets/Open-Status.png"
+        : "./assets/Closed- Status .png";
+        
+
+const priorityClass = 
+    element.priority === "high" ? "badge badge-soft badge-error"  :
+    element.priority === "medium" ? "badge badge-soft badge-warning" :
+    "badge badge-soft badge-success";
+
+
+
+        const card = document.createElement("div");
+ card.innerHTML = `
+ <div id="card" 
+                    class="card h-full bg-white shadow-lg mt-7  border-t-4 ${borderCOlor} rounded-t-xl"onclick="loadModal(${element.id})">
+                        <div class="card-top space-y-2 p-4  ">
+                            <div class="card-badge flex items-center justify-between">
+                                <div class="img">
+                                    <img src="${openClosedIcon}" alt="">
+                                </div>
+                                <div class="badge">
+                                    <div  class="${priorityClass}">${element.priority} </div>
+                                </div>
+                            </div>
+                            <h2 class="card-title font-semibold line-clamp-2 cursor-pointer"onclick="loadModal(${element.id})">
+                                ${element.title}
+                            </h2>
+                            <p class="line-clamp-2 ">${element.description}</p>
+                            <div class="card-actions">
+                            
+                                ${element.labels.map(label => `
+                                    <div class="${getLabelClass(label)} flex items-center gap-2">
+                                        <img src="${getLabelImage(label)}" alt=""><span>${label}</span>
+                                    </div>`).join('')}
+                            </div>
+
+                        </div>
+                        <div class="card-bottom p-4 border-t border-[#64748B] ">
+                            <div class="flex justify-between ">
+                                <div><p>By <span class="font-semibold">${element.author}</span> </p></div>
+                                <div><p>${new Date(element.createdAt).toLocaleDateString("en-US")}</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+ `
+ cardSection.appendChild(card)
+
+ issuCount.innerText = cardSection.children.length
+
+
+    });
+
+
+
+ 
+ 
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+allIssueLoad ()

e728e608537b56b52ba0fd99cdab75a73d25a69e
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-03-09 13:21:13 +0600

    modal work complete

--- script/script.js
+++ script/script.js
@@ -132,6 +132,82 @@ const priorityClass =
 }
 
 
+const loadModal = async(id) => {
+    my_modal_5.showModal() 
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+ await  fetch(url)
+    .then((res)=> res.json())
+    .then((data)=> showModal(data.data))
+     
+}
+
+/*
+{
+    "status": "success",
+    "message": "Issue fetched successfully",
+    "data": {
+        "id": 1,
+        "title": "Fix navigation menu on mobile devices",
+        "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+        "status": "open",
+        "labels": [
+            "bug",
+            "help wanted"
+        ],
+        "priority": "high",
+        "author": "john_doe",
+        "assignee": "jane_smith",
+        "createdAt": "2024-01-15T10:30:00Z",
+        "updatedAt": "2024-01-15T10:30:00Z"
+    }
+}
+*/
+
+function showModal(data) {
+   const modalAdd = document.getElementById('modal-add');
+   console.log(data);
+
+const modalStatus = data.status === "open"
+        ? "badge-success"
+        : "badge-error";
+   
+   modalAdd.innerHTML = `
+   <div class="modal-contant space-y-5">
+            <h2 class="font-bold text-2xl">${data.title}</h2>
+
+            <div class="flex items-center space-x-3">
+
+                <div class="badge ${modalStatus}">${data.status}</div>
+                <img src="./assets/Ellipse 6.png" alt="">
+                <p> Opened by ${data.author}</p>
+                <img src="./assets/Ellipse 6.png" alt="">
+                <p>${new Date(data.createdAt).toLocaleDateString("en-US")}</p>
+            </div>
+            <div class="badge flex">
+                ${data.labels.map(label => `
+                                    <div class="${getLabelClass(label)} flex items-center gap-2">
+                                        <img src="${getLabelImage(label)}" alt=""><span>${label}</span>
+                                    </div>`).join('')}
+            </div>
+            <p>${data.description}</p>
+            <div class="bg-[#F8FAFC] p-4 flex gap-3 justify-around">
+                <div class="left flex-2">
+                    <p>Assignee:</p>
+                    <h3>${data.assignee}</h3>
+                </div>
+                <div class="right flex-2">
+                    <p>Priority:</p>
+                    <div class="${getLabelClassModal(data.priority)}">${data.priority}</div>
+                </div>
+            </div>
+                </div>
+   `
+   
+        
+       my_modal_5.showModal()   
+    
+    
+}
 
 
 

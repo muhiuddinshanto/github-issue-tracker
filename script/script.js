a35256809d1a1d544af28de3eddb06e5f319729f
muhiuddinshanto <muhiuddinshanto@gmail.com>
2026-03-09 13:24:16 +0600

    search option complete

--- script/script.js
+++ script/script.js
@@ -214,6 +214,19 @@ const modalStatus = data.status === "open"
 }
 
 
+document.getElementById('btn-search').addEventListener("click", function(){
+    const input = document.getElementById('input-search')
+    const searchValue = input.value.trim().toLowerCase();
+
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`)
+    .then((res)=> res.json())
+    .then((data)=> {
+        allIssueDisplay(data.data)
+        
+    }
+    )
+    
+})
 
 
 
